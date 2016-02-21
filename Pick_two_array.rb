cbaf9e1a9d2c456137fd0e7cf0d1826da8b049b1
Aaron Hill <aaronthill16@gmail.com>
2016-02-21 11:23:40 -0500

    Displays groups correctly with odd number of people

--- Pick_two_array.rb
+++ Pick_two_array.rb
@@ -1,17 +1,16 @@
 def pair_generator
-	array = ["Dolly", "Aaron", "John", "Rich", "Shane", "Ed", "Jennifer", "Heather"]
-	# while array.count >= 2
-		group_1_array = []
+	array = ["Dolly", "Aaron", "John", "Rich", "Shane", "Ed", "Jennifer", "Heather", "Jenny"]
+	while array.count >= 2
 		person_one = array.sample
-		group_1_array.push(person_one)
 		array.delete(person_one)
 		person_two = array.sample
-		group_1_array.push(person_two)
-		# group_1 = person_one + person_two
 		array.delete(person_two)
-		puts group_1_array
-		puts group_1_array.count
-	# end
+		group_1_array = [person_one, person_two]
+		if array.count == 1
+			group_1_array.push(array.sample)
+		end
+		puts group_1_array.to_s
+	end 
 end
 
 pair_generator
