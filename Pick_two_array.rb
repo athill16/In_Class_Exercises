0dd7c113b7ce2fabd82674cf203ae18dd0a3cb8d
Aaron Hill <aaronthill16@gmail.com>
2016-02-21 10:44:38 -0500

    Displays four random groups

--- Pick_two_array.rb
+++ Pick_two_array.rb
@@ -1,11 +1,13 @@
 def pair_generator
 	array = ["Dolly", "Aaron", "John", "Rich", "Shane", "Ed", "Jennifer", "Heather"]
+	while array.count >= 2
 	person_one = array.sample
 	array.delete(person_one)
 	person_two = array.sample
 	group_1 = person_one + " " + person_two
 	array.delete(person_two)
 	puts group_1
+	end
 end
 
 pair_generator
