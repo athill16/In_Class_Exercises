aad3ae0dfe7d933cce3ed6911342fafadd18d783
Aaron Hill <aaronthill16@gmail.com>
2016-02-21 11:02:54 -0500

    Changed groups into arrays

--- Pick_two_array.rb
+++ Pick_two_array.rb
@@ -1,13 +1,17 @@
 def pair_generator
 	array = ["Dolly", "Aaron", "John", "Rich", "Shane", "Ed", "Jennifer", "Heather"]
-	while array.count >= 2
-	person_one = array.sample
-	array.delete(person_one)
-	person_two = array.sample
-	group_1 = person_one + " " + "and" + " " + person_two
-	array.delete(person_two)
-	puts group_1
-	end
+	# while array.count >= 2
+		group_1_array = []
+		person_one = array.sample
+		group_1_array.push(person_one)
+		array.delete(person_one)
+		person_two = array.sample
+		group_1_array.push(person_two)
+		# group_1 = person_one + person_two
+		array.delete(person_two)
+		puts group_1_array
+		puts group_1_array.count
+	# end
 end
 
 pair_generator
